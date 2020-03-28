fe36d3fed459976576cad8708ef274a60b3f0ed9
Anthony Marcar <anthony@moocar.me>
2020-03-28 13:38:45 +1100

    limit number of players who can join

--- src/ra/model/game.clj
+++ src/ra/model/game.clj
@@ -10,32 +10,40 @@
             [ra.specs.tile :as tile]
             [ra.model.player :as m-player]))
 
-(pc/defmutation new-game [{:keys [::db/conn]} {:keys [::game/num-players]}]
-  {::pc/params [::game/num-players]
+(pc/defmutation new-game [{:keys [::db/conn]} _]
+  {::pc/params []
    ::pc/output [::game/id]}
   (let [tile-bag (d/q '[:find [?t ...]
                         :where [?t ::tile/type]]
                       @conn)
         entity   {::game/id          (db/uuid)
-                  ::game/num-players num-players
                   ::game/tile-bag    tile-bag}]
     (d/transact! conn [entity])
     entity))
 
+(defn find-num-players [conn game-id]
+  (d/q '[:find (count ?pid) .
+         :in $ ?game-id
+         :where [?gid ::game/id ?game-id]
+         [?gid ::game/players ?pid]]
+       @conn
+       game-id))
+
 (pc/defmutation join-game [{:keys [::db/conn]} {game-id ::game/id player-id ::player/id}]
   {::pc/params [::game/id ::player/id]
    ::pc/output []}
-  (d/transact! conn [[:db/add [::game/id game-id] ::game/players [::player/id player-id]]])
+  (if (>= (find-num-players conn game-id) 5)
+    (throw (ex-info "Maximum players already reached" {}))
+    (d/transact! conn [[:db/add [::game/id game-id] ::game/players [::player/id player-id]]]))
   {})
 
 (pc/defresolver game-resolver [{:keys [::db/conn ::p/parent-query]}
                                {:keys [::game/id]}]
   {::pc/input #{::game/id}
-   ::pc/output [::game/num-players
-                {::game/tile-bag m-tile/q}
+   ::pc/output [{::game/tile-bag m-tile/q}
                 {::game/players m-player/q}
                 ::game/id]}
-  (d/pull (d/db conn) parent-query [::game/id id]))
+  (d/pull @conn parent-query [::game/id id]))
 
 (defmethod ig/init-key ::ref-data [_ {:keys [::db/conn]}]
   (let [tiles (d/q '[:find ?t
