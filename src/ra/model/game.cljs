9e9c65411648d513e02c4d56ad63c2eeabc24b4c
Anthony Marcar <anthony@moocar.me>
2020-03-29 20:52:48 +1100

    update history url when create game

--- src/ra/model/game.cljs
+++ src/ra/model/game.cljs
@@ -1,14 +1,33 @@
 (ns ra.model.game
   (:require [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
             [com.fulcrologic.fulcro.components :as comp]
-            [com.fulcrologic.fulcro.algorithms.data-targeting :as dt]
-            [com.fulcrologic.fulcro.routing.dynamic-routing :as dr]))
+            [ra.specs.game :as game]))
 
 (defn game-component []
   (comp/registry-key->class :ra.app.client/Game))
 
 (defmutation new-game [_]
-  (remote [{:keys [ast state] :as env}]
+  (remote [env] true
           (-> env
               (m/returning (game-component))
-              (m/with-target [:current-game]))))
+              (m/with-target [:current-game])))
+  (ok-action [{:keys [result] :as env}]
+             (let [game-id (get-in result [:body `new-game ::game/id])]
+               (-> js/window
+                   .-history
+                   (.pushState #js{:game-id game-id}
+                               "Ra in progress"
+                               (str "/game/" (str game-id)))))))
+
+(defmutation join-game [_]
+  (remote [env] true
+          (-> env
+              (m/returning (game-component))
+              (m/with-target [:current-game])))
+  (ok-action [{:keys [result] :as env}]
+             (let [game-id (get-in result [:body `new-game ::game/id])]
+               (-> js/window
+                   .-history
+                   (.pushState #js{:game-id game-id}
+                               "Ra in progress"
+                               (str "/game/" (str game-id)))))))
