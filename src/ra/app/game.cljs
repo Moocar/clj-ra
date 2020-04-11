29dd562d0464da339ef16db02878a7860ee8aa13
Anthony Marcar <anthony@moocar.me>
2020-04-12 06:52:11 +1000

    ui refresh

--- src/ra/app/game.cljs
+++ src/ra/app/game.cljs
@@ -4,6 +4,8 @@
             [com.fulcrologic.fulcro.application :as app]
             [com.fulcrologic.semantic-ui.elements.button.ui-button :refer [ui-button]]
             [com.fulcrologic.semantic-ui.collections.form.ui-form :refer [ui-form]]
+            [com.fulcrologic.semantic-ui.elements.segment.ui-segment :refer [ui-segment]]
+            [com.fulcrologic.semantic-ui.elements.segment.ui-segment-group :refer [ui-segment-group]]
             [com.fulcrologic.semantic-ui.elements.label.ui-label :refer [ui-label]]
             [com.fulcrologic.semantic-ui.elements.input.ui-input :refer [ui-input]]
             [com.fulcrologic.semantic-ui.collections.form.ui-form-field :refer [ui-form-field]]
@@ -28,6 +30,12 @@
             [com.fulcrologic.fulcro.data-fetch :as df]
             [ra.specs.hand :as hand]))
 
+(defn ui-sun-disk [v]
+  (ui-label {:circular true
+             :color    "brown"
+             :key      v}
+            v))
+
 (defsc Tile [_ {:keys [::tile/title] :as tile}]
   {:query [::tile/id
            ::tile/title
@@ -42,7 +50,7 @@
 (defsc Player [_ {:keys [::player/name]}]
   {:query [::player/name
            ::player/id]}
-  (dom/p (dom/strong name)))
+  (dom/strong name))
 
 (def ui-player (comp/factory Player {:keyfn ::player/id}))
 
@@ -59,11 +67,14 @@
            {::hand/tiles (comp/get-query Tile)}
            {::hand/player (comp/get-query Player)}]
    :ident ::hand/id}
-  (dom/div {}
-    (ui-player player)
-    (dom/p "seat: " seat)
-    (dom/p (str "Available sun disks: " (str/join ", " available-sun-disks)))
-    (ui-card-group {} (map ui-tile tiles))
+  (ui-segment {}
+              (dom/span (ui-player player) " - "
+                        (str "seat: " seat))
+    (ui-segment {:compact true}
+      (dom/div {}
+        (map ui-sun-disk available-sun-disks)))
+    (ui-segment {:compact true}
+                (ui-card-group {} (map ui-tile tiles)))
     (when my-go?
       (dom/div {}
         (ui-button {:style   {:marginTop "10"}
@@ -104,18 +115,26 @@
    :ident ::epoch/id}
   (dom/div {}
     (dom/p (str "Epoch: " number))
-    (dom/p (str "Middle Sun Disk: " current-sun-disk))
+    (ui-segment {:compact true}
+                (dom/strong "Ra track")
+                (ui-card-group {} (map ui-tile ra-tiles)))
+    (ui-segment {:compact "true"}
+                (ui-sun-disk current-sun-disk))
+    (ui-segment {:compact true}
+                (dom/strong "Aucion track")
+                (ui-card-group {} (map ui-tile auction-tiles)))
     (when auction
       (ui-auction auction))
-    (ui-card-group {} (map ui-tile ra-tiles))
-    (ui-card-group {} (map ui-tile auction-tiles))
-    (dom/ul
-     (map (fn [{:keys [::hand/seat] :as hand}]
-            (if (= seat (::hand/seat current-hand))
-              (dom/div {:style {:backgroundColor "pink"}}
-                (ui-hand hand))
-              (ui-hand hand)))
-          hands))))
+    (ui-segment {:compact true}
+                (dom/h3 "Seats")
+                (ui-segment-group {:horizontal true}
+                                  (map (fn [{:keys [::hand/seat] :as hand}]
+                                         (ui-hand hand)
+                        #_(if (= seat (::hand/seat current-hand))
+                          (ui-segment dom/div {:style {:backgroundColor "pink"}}
+                            (ui-hand hand))
+                          (ui-hand hand)))
+                      hands)))))
 
 (def ui-epoch (comp/factory Epoch {:keyfn ::epoch/number}))
 
@@ -137,7 +156,6 @@
       (dom/div {}
         (if started-at
           (dom/div {}
-            (dom/p "game started")
             (ui-epoch current-epoch))
           (ui-button {:primary true
                       :onClick (fn []
